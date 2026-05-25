eb1e8290c55e5b98de30a44bbeb4ab49613d9f00
Sebastián León M. <sleonmedellin@gmail.com>
2026-05-25 13:35:47 -0600

    Separate the ip's from the public project

--- PLC-MQTT-Gateway/gateway.py
+++ PLC-MQTT-Gateway/gateway.py
@@ -1,36 +1,29 @@
+from config import *
+
 import snap7
 from snap7.util import get_int, get_bool
 import paho.mqtt.client as mqtt
 import time
 
 # PLC CONFIG
-
-PLC_IP = "192.168.0.3"
-
 DB_NUMBER = 2
 PLC_START_ADDRESS = 0
 PLC_SIZE = 3   # bytes a leer
 
 # MQTT CONFIG
-
-MQTT_BROKER = "10.25.110.236"   # IP de la PC con Mosquitto
-MQTT_PORT = 1883
 TOPIC = "yopublico"
 
 # INIT PLC
-
 plc = snap7.client.Client()
 plc.connect(PLC_IP, 0, 1)
 print("Conectado al PLC")
 
 # INIT MQTT
-
 client = mqtt.Client()
 client.connect(MQTT_BROKER, MQTT_PORT, 60)
 print("Conectado al broker MQTT")
 
 # MAIN LOOP
-
 while True:
     # Leer bytes del DB
     data = plc.db_read(DB_NUMBER, PLC_START_ADDRESS, PLC_SIZE)
