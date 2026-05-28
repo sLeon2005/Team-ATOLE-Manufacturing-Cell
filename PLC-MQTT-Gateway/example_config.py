e0d89d1965c6ad812257bca5b1011c7d7947f804
Sebastián León M. <sleonmedellin@gmail.com>
2026-05-28 08:39:03 -0600

    added example ip's file

---
+++ PLC-MQTT-Gateway/example_config.py
@@ -0,0 +1,8 @@
+# Example configuration file
+# Rename this file to "config.py"
+# Modify the values below to match your PLC and MQTT broker settings
+
+PLC_IP = "192.168.X.X"
+
+MQTT_BROKER = "192.168.X.X"
+MQTT_PORT = 1883
